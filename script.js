bb942364daf6ec0f22fb230034c151a0f6245f3a
Lucas Mendes Sales <lucasmendessales@pop-os.localdomain>
2021-12-26 15:05:13 -0300

    base project - image pieces created

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // this function runs when the DOM is ready, i.e. when the document has been parsed
+    document.querySelector("#hello__container").style.backgroundColor = "red";
+    document.querySelector("#hello").textContent = "Hello World!";
+});
