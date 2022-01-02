c79371025059161f66ad4109fc638f909b21d891
Lucas Mendes Sales <lucasmendessales@pop-os.localdomain>
2022-01-02 15:54:15 -0300

    adding functions swapPiece, getPossibleBoards, checkVisitedNodes, printBoard + adding priorityQueue

--- solver.js
+++ solver.js
@@ -1,16 +1,81 @@
 import checkMovebility from "./checkMovebility.js";
 
-//const missingPiece = piece
-
-//map = { piece => position}
-
-//initial state => object of pieces translated using the map
-
-//import function that tells the possible movements
-
-let board = [[{ piece: { x: 0, y: 1 }, distance: 0 }, { piece: { x: 1, y: 1 }, distance: 0 }, { piece: { x: 0, y: 0 }, distance: 0 }],
+//to do: get this board from the "real board" state
+let initialBoard = [[{ piece: { x: 0, y: 1 }, distance: 0 }, { piece: { x: 1, y: 1 }, distance: 0 }, { piece: { x: 0, y: 0 }, distance: 0 }],
 [{ piece: { x: 2, y: 1 }, distance: 0 }, { piece: { x: 1, y: 2 }, distance: 0 }, { piece: null, distance: 0 }],
 [{ piece: { x: 0, y: 2 }, distance: 0 }, { piece: { x: 1, y: 0 }, distance: 0 }, { piece: { x: 2, y: 0 }, distance: 0 }]];
+
+//to do: change this to visited boards
+let visitedNodes = [];
+
+//src: https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript
+const top = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+class PriorityQueue {
+
+    constructor(comparator = (a, b) => a > b) {
+        this._heap = [];
+        this._comparator = comparator;
+    }
+    size() {
+        return this._heap.length;
+    }
+    isEmpty() {
+        return this.size() == 0;
+    }
+    peek() {
+        return this._heap[top];
+    }
+    push(...values) {
+        values.forEach(value => {
+            this._heap.push(value);
+            this._siftUp();
+        });
+        return this.size();
+    }
+    pop() {
+        const poppedValue = this.peek();
+        const bottom = this.size() - 1;
+        if (bottom > top) {
+            this._swap(top, bottom);
+        }
+        this._heap.pop();
+        this._siftDown();
+        return poppedValue;
+    }
+    replace(value) {
+        const replacedValue = this.peek();
+        this._heap[top] = value;
+        this._siftDown();
+        return replacedValue;
+    }
+    _greater(i, j) {
+        return this._comparator(this._heap[i], this._heap[j]);
+    }
+    _swap(i, j) {
+        [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+    }
+    _siftUp() {
+        let node = this.size() - 1;
+        while (node > top && this._greater(node, parent(node))) {
+            this._swap(node, parent(node));
+            node = parent(node);
+        }
+    }
+    _siftDown() {
+        let node = top;
+        while (
+            (left(node) < this.size() && this._greater(left(node), node)) ||
+            (right(node) < this.size() && this._greater(right(node), node))
+        ) {
+            let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+            this._swap(node, maxChild);
+            node = maxChild;
+        }
+    }
+}
 
 function calcDistance(currentPosition, rightPosition) {
     return Math.sqrt(Math.pow((currentPosition.x - rightPosition.x), 2))
@@ -18,7 +83,7 @@ function calcDistance(currentPosition, rightPosition) {
 }
 
 function setDistances(board) {
-    for (let i = 0; i < board[0].length; i++) {
+    for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[0].length; j++) {
             if (board[i][j].piece !== null) board[i][j].distance = calcDistance({ x: i, y: j }, board[i][j].piece);
         }
@@ -63,7 +128,7 @@ function pieceStringToCoordinates(piece) {
             y = 2;
     }
 
-    return {x: x, y: y};
+    return { x: x, y: y };
 }
 
 function pieceCoordinatesToString(piece) {
@@ -95,14 +160,51 @@ function pieceCoordinatesToString(piece) {
     return `${x} ${y}`;
 }
 
-function checkVisitedNodes() {
-    //check the visited nodes at the knowledge bank
+//to do: optimize this function
+function checkVisitedNode(board) {
+
+    for (let visIndex = 0; visIndex < visitedNodes.length; visIndex++) {
+
+        let visited = visitedNodes[visIndex];
+        let wasVisited = true;
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[0].length; j++) {
+
+                if (visited[i][j].piece === null && board[i][j].piece === null) continue
+                else if (visited[i][j].piece === null || board[i][j].piece === null) {
+                    wasVisited = false;
+                    break;
+                } else if ((visited[i][j].piece.x !== board[i][j].piece.x) || visited[i][j].piece.y !== board[i][j].piece.y) {
+                    wasVisited = false;
+                    break;
+                }
+            }
+        }
+
+        if (wasVisited) return true;
+    }
+
+    return false;
+}
+
+function printBoard(board) {
+    for (let i = 0; i < board.length; i++) {
+        let line = " | "
+        for (let j = 0; j < board[0].length; j++) {
+            let piece = board[i][j].piece;
+            if (piece !== null) line += `(${piece.x},${piece.y}) |`
+            else line += "(_,_) |"
+        }
+        console.log(line);
+    }
 }
 
 class Node {
 
-    constructor(board, freePlace, path) {
-        this.fullDistance = calcFullDistance(board);
+    constructor(board, freePlace, fullDistance, path) {
+        this.board = board;
+        this.fullDistance = fullDistance;
         this.lineEnd = false;
         this.path = path;
         this.freePlace = freePlace;
@@ -111,11 +213,10 @@ class Node {
     getMoveblePieces() {
         let res = [];
 
-        for(let i = 0; i < board[0].length; i++){
-            for(let j = 0; j < board.length; j++){
-                let piece = {x: i, y: j};
-                console.log(piece);
-                if(piece !== this.freePlace && checkMovebility( pieceCoordinatesToString(piece), pieceCoordinatesToString(this.freePlace) ))
+        for (let i = 0; i < this.board[0].length; i++) {
+            for (let j = 0; j < this.board.length; j++) {
+                let piece = { x: i, y: j };
+                if (piece !== this.freePlace && checkMovebility(pieceCoordinatesToString(piece), pieceCoordinatesToString(this.freePlace)))
                     res.push(piece);
             }
         }
@@ -123,25 +224,63 @@ class Node {
         return res;
     }
 
-    createChild() {
-        //calc next possible boards (check wich pieces can be swapped, create a board for each possibility swapping the piece with the free space)
+    swapPiece(piece) {
 
-        //select the board with the minimum distance
-        //create child node
-        //update knowledge bank
-        //return node
+        let newBoard = this.board.map(function (arr) {
+            return arr.slice();
+        });
+
+        newBoard[this.freePlace.x][this.freePlace.y] = newBoard[piece.x][piece.y];
+        newBoard[piece.x][piece.y] = { piece: null, distance: 0 }
+
+        setDistances(newBoard);
+
+        return { newBoard: newBoard, newFreePlace: piece, newFullDistance: calcFullDistance(newBoard) }
+
+    }
+
+    getPossibleBoards() {
+        const moveblePieces = this.getMoveblePieces();
+        const possibleBoards = moveblePieces.map(piece => {
+            return this.swapPiece(piece);
+        });
+        possibleBoards.forEach(pb => {
+            setDistances(pb.newBoard);
+        });
+
+        return possibleBoards;
     }
 
 }
 
-function solvePuzzle(initialState) {
-    //create first node
-    //loop until full distance = 0
-    //create next node, 
-    //
+function solvePuzzle(initialBoard) {
+    const nextNodesQueue = new PriorityQueue((node1, node2) => node1.fullDistance < node2.fullDistance);
+    setDistances(initialBoard);
+    let currentNode = new Node(initialBoard, { x: 1, y: 2 }, calcFullDistance(initialBoard), "");
+
+    // for(let i = 0; i < 10000; i++){
+    while (currentNode.fullDistance !== 0) {
+
+        visitedNodes.push(currentNode.board);
+        const possibleBoards = currentNode.getPossibleBoards();
+        const possibleNodes = possibleBoards.map(pb => {
+            return new Node(pb.newBoard, pb.newFreePlace, pb.newFullDistance, "");
+        });
+
+        nextNodesQueue.push(...possibleNodes);
+        currentNode = nextNodesQueue.pop();
+        while (checkVisitedNode(currentNode.board)) {
+            currentNode = nextNodesQueue.pop();
+        }
+    }
+
+    printBoard(currentNode.board);
     //translate current node's path to class names 
     //return te path
 }
 
-const node = new Node(board, {x: 1, y: 2}, "");
-console.log(node.getMoveblePieces());
+solvePuzzle(initialBoard);
+
+
+
+
